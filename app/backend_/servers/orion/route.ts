316f1f7150bbf3fd05b5e8303c71c328e504b7f3
jerometecson-main <zxctest8@zxcstream.xyz>
2026-06-28 02:06:43 +0800

    more proxy

--- app/backend_/servers/orion/route.ts
+++ app/backend_/servers/orion/route.ts
@@ -245,7 +245,14 @@ const supabase = createClient(
 //https://holly-1.test66-8cc.workers.dev/ https://holly-1.test68-6e8.workers.dev/ https://holly-1.test67-989.workers.dev/ https://holly-1.test65-8de.workers.dev/
 //https://holly-1.test70-ee3.workers.dev/ https://holly-1.test69-5d6.workers.dev/ https://holly-1.test71-dc9.workers.dev/ https://holly-1.test72-165.workers.dev/
 //https://holly-1.test76-4e9.workers.dev/ https://holly-1.test75-da4.workers.dev/ https://holly-1.test74-635.workers.dev/ https://holly-1.test73-bfb.workers.dev/
+
+//https://holly-1.test80-1f4.workers.dev/ https://holly-1.test78-564.workers.dev/
+//https://holly-1.test77-a68.workers.dev/ https://holly-1.test79-29a.workers.dev/
 const HOLLY_WORKERS = [
+  "test80-1f4",
+  "test79-29a",
+  "test78-564",
+  "test77-a68",
   "test76-4e9",
   "test75-da4",
   "test74-635",
