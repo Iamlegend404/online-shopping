25d4678aebb13c8dbd5269b074015a510945f778
jerometecson-main <zxctest8@zxcstream.xyz>
2026-07-25 22:25:11 +0800

    v7

--- lib/server-list.ts
+++ lib/server-list.ts
@@ -3,7 +3,7 @@ import { ServerTypes } from "@/types/player-types";
 export const initialServers: ServerTypes[] = [
   {
     name: "Resshin II",
-    server: "resshin_",
+    server: "resshin__",
     status: "queue",
     desc: "Download & Multi Audio Support",
   },
